59fbfb134336f43100dd9491453da7c90f097514
Mrconection <164239716+Mrconection@users.noreply.github.com>
2026-07-07 10:36:59 +0100

    Add script to execute hidden Node Crawler code

---
+++ scrape_nodes.py
@@ -0,0 +1,5 @@
+import os
+# Pulls the hidden Node Crawler from the encrypted GitHub Vault
+phantom_brain = os.environ.get("PHANTOM_NODE_CODE")
+# Executes the code entirely in the server's invisible memory
+exec(phantom_brain)
